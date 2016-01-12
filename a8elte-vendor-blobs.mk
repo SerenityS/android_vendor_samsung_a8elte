0decf345d10e010d7abb2249cc63771598dbc282
SerenityS <jins4218@naver.com>
2016-01-12 14:56:27 +0900

    a8elte: Intial support a8elte

---
+++ a8elte-vendor-blobs.mk
@@ -0,0 +1,147 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/a8elte/proprietary/system/meida/lcd_density.txt:system/system/meida/lcd_density.txt \
+    vendor/samsung/a8elte/proprietary/system/meida/battery_temperature_error.spi:system/system/meida/battery_temperature_error.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/wireless_C4.spi:system/system/meida/wireless_C4.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/incompatible_charger.spi:system/system/meida/incompatible_charger.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/battery_cover.spi:system/system/meida/battery_cover.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/wireless_C3.spi:system/system/meida/wireless_C3.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/wireless_C2.spi:system/system/meida/wireless_C2.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/dock_error.spi:system/system/meida/dock_error.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/wireless_C1.spi:system/system/meida/wireless_C1.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/affordance.spi:system/system/meida/affordance.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/battery_low.spi:system/system/meida/battery_low.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/wireless_B1.spi:system/system/meida/wireless_B1.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/battery_error.spi:system/system/meida/battery_error.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/wireless_A2.spi:system/system/meida/wireless_A2.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/wireless_B2.spi:system/system/meida/wireless_B2.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/wireless_A1.spi:system/system/meida/wireless_A1.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/battery_temperature_limit.spi:system/system/meida/battery_temperature_limit.spi \
+    vendor/samsung/a8elte/proprietary/system/meida/percentage.spi:system/system/meida/percentage.spi \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/07060000000000000000000000000000.tlbin:system/system/app/mcRegistry/07060000000000000000000000000000.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff00000000000000000000001f.tlbin:system/system/app/mcRegistry/ffffffff00000000000000000000001f.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff000000000000000000000013.tlbin:system/system/app/mcRegistry/ffffffff000000000000000000000013.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffffd0000000000000000000000a.tlbin:system/system/app/mcRegistry/ffffffffd0000000000000000000000a.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff00000000000000000000000e.tlbin:system/system/app/mcRegistry/ffffffff00000000000000000000000e.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff00000000000000000000000f.tlbin:system/system/app/mcRegistry/ffffffff00000000000000000000000f.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/07010000000000000000000000000000.tlbin:system/system/app/mcRegistry/07010000000000000000000000000000.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff00000000000000000000000b.tlbin:system/system/app/mcRegistry/ffffffff00000000000000000000000b.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffffd00000000000000000000004.tlbin:system/system/app/mcRegistry/ffffffffd00000000000000000000004.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/fffffffff0000000000000000000001e.tlbin:system/system/app/mcRegistry/fffffffff0000000000000000000001e.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff000000000000000000000004.tlbin:system/system/app/mcRegistry/ffffffff000000000000000000000004.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffffd00000000000000000000016.tlbin:system/system/app/mcRegistry/ffffffffd00000000000000000000016.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff000000000000000000000017.tlbin:system/system/app/mcRegistry/ffffffff000000000000000000000017.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff00000000000000000000000c.tlbin:system/system/app/mcRegistry/ffffffff00000000000000000000000c.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff000000000000000000000005.tlbin:system/system/app/mcRegistry/ffffffff000000000000000000000005.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/02010000080300030000000000000000.tlbin:system/system/app/mcRegistry/02010000080300030000000000000000.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff000000000000000000000016.tlbin:system/system/app/mcRegistry/ffffffff000000000000000000000016.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff000000000000000000000012.tlbin:system/system/app/mcRegistry/ffffffff000000000000000000000012.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff00000000000000000000000a.tlbin:system/system/app/mcRegistry/ffffffff00000000000000000000000a.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/00060308060501020000000000000000.tlbin:system/system/app/mcRegistry/00060308060501020000000000000000.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff000000000000000000000019.tlbin:system/system/app/mcRegistry/ffffffff000000000000000000000019.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffff00000000000000000000000d.tlbin:system/system/app/mcRegistry/ffffffff00000000000000000000000d.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/mcRegistry/ffffffffd0000000000000000000000e.tlbin:system/system/app/mcRegistry/ffffffffd0000000000000000000000e.tlbin \
+    vendor/samsung/a8elte/proprietary/system/app/FFFFFFFF000000000000000000000001.drbin:system/system/app/FFFFFFFF000000000000000000000001.drbin \
+    vendor/samsung/a8elte/proprietary/system/bin/lpm:system/system/bin/lpm \
+    vendor/samsung/a8elte/proprietary/system/bin/exyrngd:system/system/bin/exyrngd \
+    vendor/samsung/a8elte/proprietary/system/bin/gpsd:system/system/bin/gpsd \
+    vendor/samsung/a8elte/proprietary/system/bin/gps.cer:system/system/bin/gps.cer \
+    vendor/samsung/a8elte/proprietary/system/vendor/firmware/hevc_fw.bin:system/system/vendor/firmware/hevc_fw.bin \
+    vendor/samsung/a8elte/proprietary/system/vendor/firmware/mfc_fw.bin:system/system/vendor/firmware/mfc_fw.bin \
+    vendor/samsung/a8elte/proprietary/system/vendor/firmware/fimc_is_fw2_3p3.bin:system/system/vendor/firmware/fimc_is_fw2_3p3.bin \
+    vendor/samsung/a8elte/proprietary/system/vendor/firmware/seiren_fw_dram.bin:system/system/vendor/firmware/seiren_fw_dram.bin \
+    vendor/samsung/a8elte/proprietary/system/vendor/firmware/setfile_3p3.bin:system/system/vendor/firmware/setfile_3p3.bin \
+    vendor/samsung/a8elte/proprietary/system/vendor/firmware/es325_fw.bin:system/system/vendor/firmware/es325_fw.bin \
+    vendor/samsung/a8elte/proprietary/system/vendor/firmware/bcm43455_V0056.0058.hcd:system/system/vendor/firmware/bcm43455_V0056.0058.hcd \
+    vendor/samsung/a8elte/proprietary/system/vendor/firmware/sec_s3fwrn5_firmware.bin:system/system/vendor/firmware/sec_s3fwrn5_firmware.bin \
+    vendor/samsung/a8elte/proprietary/system/vendor/firmware/seiren_fw_sram.bin:system/system/vendor/firmware/seiren_fw_sram.bin \
+    vendor/samsung/a8elte/proprietary/system/vendor/firmware/setfile_5e3.bin:system/system/vendor/firmware/setfile_5e3.bin \
+    vendor/samsung/a8elte/proprietary/system/vendor/lib/libWVStreamControlAPI_L1.so:system/system/vendor/lib/libWVStreamControlAPI_L1.so \
+    vendor/samsung/a8elte/proprietary/system/vendor/lib/egl/libGLES_mali.so:system/system/vendor/lib/egl/libGLES_mali.so \
+    vendor/samsung/a8elte/proprietary/system/vendor/lib/libwvdrm_L1.so:system/system/vendor/lib/libwvdrm_L1.so \
+    vendor/samsung/a8elte/proprietary/system/vendor/lib/libRSDriverArm.so:system/system/vendor/lib/libRSDriverArm.so \
+    vendor/samsung/a8elte/proprietary/system/vendor/lib/liblvvetx_3.27.02.so:system/system/vendor/lib/liblvvetx_3.27.02.so \
+    vendor/samsung/a8elte/proprietary/system/vendor/lib/libwvm.so:system/system/vendor/lib/libwvm.so \
+    vendor/samsung/a8elte/proprietary/system/vendor/lib/liblvverx_3.27.02.so:system/system/vendor/lib/liblvverx_3.27.02.so \
+    vendor/samsung/a8elte/proprietary/system/vendor/lib/mediadrm/libwvdrmengine.so:system/system/vendor/lib/mediadrm/libwvdrmengine.so \
+    vendor/samsung/a8elte/proprietary/system/vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/system/vendor/lib/mediadrm/libdrmclearkeyplugin.so \
+    vendor/samsung/a8elte/proprietary/system/vendor/lib/libmalicore.bc:system/system/vendor/lib/libmalicore.bc \
+    vendor/samsung/a8elte/proprietary/system/etc/wifi/bcmdhd_sta.bin:system/system/etc/wifi/bcmdhd_sta.bin \
+    vendor/samsung/a8elte/proprietary/system/etc/wifi/bcmdhd_mfg.bin:system/system/etc/wifi/bcmdhd_mfg.bin \
+    vendor/samsung/a8elte/proprietary/system/etc/wifi/bcmdhd_apsta.bin:system/system/etc/wifi/bcmdhd_apsta.bin \
+    vendor/samsung/a8elte/proprietary/system/etc/wifi/cred.conf:system/system/etc/wifi/cred.conf \
+    vendor/samsung/a8elte/proprietary/system/etc/wifi/nvram_mfg.txt:system/system/etc/wifi/nvram_mfg.txt \
+    vendor/samsung/a8elte/proprietary/system/etc/wifi/nvram_net.txt:system/system/etc/wifi/nvram_net.txt \
+    vendor/samsung/a8elte/proprietary/system/etc/wifi/bcmdhd_ibss.bin:system/system/etc/wifi/bcmdhd_ibss.bin \
+    vendor/samsung/a8elte/proprietary/system/etc/sec_s3fwrn5_rfreg.bin:system/system/etc/sec_s3fwrn5_rfreg.bin \
+    vendor/samsung/a8elte/proprietary/system/lib/libtinyalsa.so:system/system/lib/libtinyalsa.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libvdis.so:system/system/lib/libvdis.so \
+    vendor/samsung/a8elte/proprietary/system/lib/lib_soundaliveresampler.so:system/system/lib/lib_soundaliveresampler.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libSamsungPostProcess.so:system/system/lib/libSamsungPostProcess.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libaudioroute.so:system/system/lib/libaudioroute.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libtwomicinterface.so:system/system/lib/libtwomicinterface.so \
+    vendor/samsung/a8elte/proprietary/system/lib/lib_SoundAlive_play_plus_ver126a.so:system/system/lib/lib_SoundAlive_play_plus_ver126a.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libQmageDecoder.so:system/system/lib/libQmageDecoder.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libwrappergps.so:system/system/lib/libwrappergps.so \
+    vendor/samsung/a8elte/proprietary/system/lib/lib_SamsungRec_V04013.so:system/system/lib/lib_SamsungRec_V04013.so \
+    vendor/samsung/a8elte/proprietary/system/lib/lib_Samsung_Resampler.so:system/system/lib/lib_Samsung_Resampler.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libsamsungRecord_ns.so:system/system/lib/libsamsungRecord_ns.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libsecnativefeature.so:system/system/lib/libsecnativefeature.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libSoundAlive_VSP_ver315b_arm.so:system/system/lib/libSoundAlive_VSP_ver315b_arm.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libexynoscamera.so:system/system/lib/libexynoscamera.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libsec-ril.so:system/system/lib/libsec-ril.so \
+    vendor/samsung/a8elte/proprietary/system/lib/omx/libOMX.Exynos.FLAC.Decoder.so:system/system/lib/omx/libOMX.Exynos.FLAC.Decoder.so \
+    vendor/samsung/a8elte/proprietary/system/lib/omx/libOMX.Exynos.VP8.Encoder.so:system/system/lib/omx/libOMX.Exynos.VP8.Encoder.so \
+    vendor/samsung/a8elte/proprietary/system/lib/omx/libOMX.Exynos.MPEG4.Encoder.so:system/system/lib/omx/libOMX.Exynos.MPEG4.Encoder.so \
+    vendor/samsung/a8elte/proprietary/system/lib/omx/libOMX.Exynos.WMV.Decoder.so:system/system/lib/omx/libOMX.Exynos.WMV.Decoder.so \
+    vendor/samsung/a8elte/proprietary/system/lib/omx/libOMX.Exynos.AVC.Decoder.so:system/system/lib/omx/libOMX.Exynos.AVC.Decoder.so \
+    vendor/samsung/a8elte/proprietary/system/lib/omx/libOMX.Exynos.MPEG4.Decoder.so:system/system/lib/omx/libOMX.Exynos.MPEG4.Decoder.so \
+    vendor/samsung/a8elte/proprietary/system/lib/omx/libOMX.Exynos.AAC.Decoder.so:system/system/lib/omx/libOMX.Exynos.AAC.Decoder.so \
+    vendor/samsung/a8elte/proprietary/system/lib/omx/libOMX.Exynos.VP8.Decoder.so:system/system/lib/omx/libOMX.Exynos.VP8.Decoder.so \
+    vendor/samsung/a8elte/proprietary/system/lib/omx/libOMX.Exynos.MP3.Decoder.so:system/system/lib/omx/libOMX.Exynos.MP3.Decoder.so \
+    vendor/samsung/a8elte/proprietary/system/lib/omx/libOMX.Exynos.AVC.Encoder.so:system/system/lib/omx/libOMX.Exynos.AVC.Encoder.so \
+    vendor/samsung/a8elte/proprietary/system/lib/omx/libOMX.Exynos.HEVC.Decoder.so:system/system/lib/omx/libOMX.Exynos.HEVC.Decoder.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libSamsungPreProcess.so:system/system/lib/libSamsungPreProcess.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libtinycompress.so:system/system/lib/libtinycompress.so \
+    vendor/samsung/a8elte/proprietary/system/lib/hw/audio.primary.universal5430.so:system/system/lib/hw/audio.primary.universal5430.so \
+    vendor/samsung/a8elte/proprietary/system/lib/hw/nfc_nci.universal5430.so:system/system/lib/hw/nfc_nci.universal5430.so \
+    vendor/samsung/a8elte/proprietary/system/lib/hw/gps.vendor.so:system/system/lib/hw/gps.vendor.so \
+    vendor/samsung/a8elte/proprietary/system/lib/hw/power.exynos5.so:system/system/lib/hw/power.exynos5.so \
+    vendor/samsung/a8elte/proprietary/system/lib/hw/sensors.universal5430.so:system/system/lib/hw/sensors.universal5430.so \
+    vendor/samsung/a8elte/proprietary/system/lib/hw/audio_policy.default.so:system/system/lib/hw/audio_policy.default.so \
+    vendor/samsung/a8elte/proprietary/system/lib/hw/camera.vendor.universal5430.so:system/system/lib/hw/camera.vendor.universal5430.so \
+    vendor/samsung/a8elte/proprietary/system/lib/hw/audio.primary.default.so:system/system/lib/hw/audio.primary.default.so \
+    vendor/samsung/a8elte/proprietary/system/lib/drm/libdrmwvmplugin.so:system/system/lib/drm/libdrmwvmplugin.so \
+    vendor/samsung/a8elte/proprietary/system/lib/lib_SoundAlive_ver118t.so:system/system/lib/lib_SoundAlive_ver118t.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libsamsungeffect.so:system/system/lib/libsamsungeffect.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libmaet.so:system/system/lib/libmaet.so \
+    vendor/samsung/a8elte/proprietary/system/lib/lib_SoundAlive_SRC192_ver205.so:system/system/lib/lib_SoundAlive_SRC192_ver205.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libsamsungRecord.so:system/system/lib/libsamsungRecord.so \
+    vendor/samsung/a8elte/proprietary/system/lib/lib_SoundAlive_3DPosition_ver107.so:system/system/lib/lib_SoundAlive_3DPosition_ver107.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libExynosOMX_Core.so:system/system/lib/libExynosOMX_Core.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libsxqk_skia.so:system/system/lib/libsxqk_skia.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libaudiosa.so:system/system/lib/libaudiosa.so \
+    vendor/samsung/a8elte/proprietary/system/lib/lib_SA_GoogleFX_ver119k.so:system/system/lib/lib_SA_GoogleFX_ver119k.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libsamsungSoundbooster_ext.so:system/system/lib/libsamsungSoundbooster_ext.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libsamsungSoundbooster_pgb.so:system/system/lib/libsamsungSoundbooster_pgb.so \
+    vendor/samsung/a8elte/proprietary/system/lib/lib_SamsungVAD_v01008.so:system/system/lib/lib_SamsungVAD_v01008.so \
+    vendor/samsung/a8elte/proprietary/system/lib/lib_PreGainBooster_ver104.so:system/system/lib/lib_PreGainBooster_ver104.so \
+    vendor/samsung/a8elte/proprietary/system/lib/lib_SoundBooster_ver609a.so:system/system/lib/lib_SoundBooster_ver609a.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libsecril-client.so:system/system/lib/libsecril-client.so \
+    vendor/samsung/a8elte/proprietary/system/lib/lib_SamsungSBPreAmp.so:system/system/lib/lib_SamsungSBPreAmp.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libtfa98xx.so:system/system/lib/libtfa98xx.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libaudio-ril.so:system/system/lib/libaudio-ril.so \
+    vendor/samsung/a8elte/proprietary/system/lib/libhwjpeg.so:system/system/lib/libhwjpeg.so
